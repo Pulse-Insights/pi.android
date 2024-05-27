fe8879811f73421a894a11645061aa2ad7972887
shenlong <shenlong@ekohe.com>
2024-05-27 18:07:38 +0800

    MISC - Update logic

--- surveysdk/src/main/java/com/pulseinsights/surveysdk/util/SurveyAnswers.java
+++ surveysdk/src/main/java/com/pulseinsights/surveysdk/util/SurveyAnswers.java
@@ -3,6 +3,8 @@ package com.pulseinsights.surveysdk.util;
 import android.text.TextUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -14,12 +16,31 @@ public class SurveyAnswers {
     }
 
     public void setAnswer(String questionId, String questionType, String answer) {
-        if (questionType.equals("single_choice_question") || questionType.equals("free_text_question")) {
-            if (TextUtils.isEmpty(answer)) {
-                // remove answer if it is empty with it's questionId
-                answers.removeIf(surveyAnswer -> Objects.equals(surveyAnswer.getQuestionId(), questionId));
-                return;
+        if (TextUtils.isEmpty(answer)) {
+            Iterator<SurveyAnswer> iterator = answers.iterator();
+            while (iterator.hasNext()) {
+                SurveyAnswer surveyAnswer = iterator.next();
+                if (Objects.equals(surveyAnswer.getQuestionId(), questionId)) {
+                    iterator.remove();
+                }
             }
+            return;
+        }
+        if (questionType.equals("single_choice_question")) {
+            for (int i = answers.size() - 1; i >= 0; i--) {
+                SurveyAnswer existingAnswer = answers.get(i);
+                if (Objects.equals(existingAnswer.getQuestionId(), questionId)) {
+                    if (Objects.equals(existingAnswer.getAnswer(), answer)) {
+                        // If the questionId exists and the answer is the same, remove it
+                        answers.remove(i);
+                    } else {
+                        // If the questionId exists but the answer is different, replace it
+                        existingAnswer.setAnswer(answer);
+                    }
+                    return;
+                }
+            }
+        } else if (questionType.equals("free_text_question")) {
             for (SurveyAnswer surveyAnswer : answers) {
                 if (Objects.equals(surveyAnswer.getQuestionId(), questionId)) {
                     surveyAnswer.setAnswer(answer);
@@ -27,19 +48,11 @@ public class SurveyAnswers {
                 }
             }
         } else if (questionType.equals("multiple_choices_question")) {
-            if (TextUtils.isEmpty(answer)) {
-                // remove answer if it is empty with it's questionId
-                answers.removeIf(surveyAnswer -> Objects.equals(surveyAnswer.getQuestionId(), questionId));
-                return;
-            }
             // need to format 001&002 => 001,002
             answer = answer.replace("&", ",");
             for (SurveyAnswer surveyAnswer : answers) {
                 if (Objects.equals(surveyAnswer.getQuestionId(), questionId)) {
-                    String existingAnswer = surveyAnswer.getAnswer();
-                    if (!existingAnswer.contains(answer)) {
-                        surveyAnswer.setAnswer(existingAnswer + "," + answer);
-                    }
+                    surveyAnswer.setAnswer(answer);
                     return;
                 }
             }
